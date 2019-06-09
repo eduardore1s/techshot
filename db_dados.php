0b5b4d17a3f3fc8bb7ccce361d6c8de9971048a4
eduardore1s <edsilveira.reis@gmail.com>
2019-06-09 00:16:51 -0300

    banco

---
+++ db_dados.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
+
+// Parsing connnection string
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+        continue;
+    }
+    
+    $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+echo "Servidor: $servername, <br>Usuário: $username, <br>Senha: $password, <br>Database: $dbname";
+
+?>
